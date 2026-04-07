817d7b418cc6b98956f4082f94c65087bc055112
Joao Azevedo <geral.joaoazevedo12@gmail.com>
2026-04-07 09:40:07 +0100

    Add files via upload

---
+++ mads_tabacarias/mads_tabacarias/mads_tabacarias/__init__.py
@@ -0,0 +1,24 @@
+from .projeto_tabacarias import (
+    # Registos
+    registar_tabacaria,
+    registar_produto,
+    registar_utilizador,
+    registar_venda,
+
+    # Consultas
+    consultar_tabacarias,
+    consultar_vendas,
+    consultar_vendas_por_tabacaria,
+
+    # Ranking
+    ranking_tabacarias,
+
+    # Gráficos
+    grafico_vendas_tabacaria,
+    grafico_produtos,
+
+    # Mapa
+    mapa,
+    set_ponto
+)
+
